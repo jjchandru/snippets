a0e0bed43bd972a76b3323c87746039f1e862eb5
jjchandru <jjchandru@yahoo.com>
2021-10-06 22:48:49 +0530

    Create movie.ts

---
+++ angular/model/movie.ts
@@ -0,0 +1,13 @@
+import { Genre } from "./genre";
+import { MpaaRating } from "./mpaa-rating";
+
+export interface Movie {
+  id: number,
+  title: string,
+  genres: Genre[],
+  releaseDate: Date,
+  mpaaRating: MpaaRating,
+  rating: number,
+  duration: number,
+  budget: number
+}
